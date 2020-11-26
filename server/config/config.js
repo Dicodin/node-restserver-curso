0b9f77ed02aacf13400944a6e10ec1b4683c0393
Daniel <dicodin@outlook.com>
2020-11-25 19:55:39 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://user_cafe:0SiMlK9PQFfqFjZ6@dancluster.gwnky.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
