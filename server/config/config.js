612cf4fe1275822280581badce100817399ca277
Daniel <dicodin@outlook.com>
2020-12-01 19:11:27 -0600

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -38,3 +38,9 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+//==============================
+// Google Client ID
+//==============================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '220373152188-gke0f7p4ba4bo4vbi1l7as05g89lub2o.apps.googleusercontent.com';
